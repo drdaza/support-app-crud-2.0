8e0c5bcd03ab737d3f7241b6deea8a354cb2ab59
Xavi1594 <xavi9415@hotmail.com>
2023-01-24 15:29:38 +0100

    creado componente footer y añadiendo estilos

--- src/main/frontend/src/views/List.vue
+++ src/main/frontend/src/views/List.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
 </script>
 <template>
   <div class="header">
     <Header></Header>
+    <Footer></Footer>
   </div>
 </template>
 <style scoped>
