e5d8e7d7309548f265fd75b27eeac084ea53afd9
Ramiro-isla <ramiroisla11@gmail.com>
2023-01-26 21:49:52 +0100

    method post frontend  done

--- src/main/frontend/src/components/FormComponent.vue
+++ src/main/frontend/src/components/FormComponent.vue
@@ -1,15 +1,27 @@
 <script setup>
-import { ref } from 'vue';
-import request from '../API-call/requestRepository/request';
+import { ref } from "vue";
+import request from "../API-call/requestRepository/request";
 let id = ref(0);
-
-const postMethod = ()=>{
+let name = ref("");
+let date = ref("");
+let type = ref("");
+let description = ref("");
+const postMethod = () => {
+  let dateRequest = date.value.split("-");
   let temporalObject = {
-    "id": id.value,
-  
-  }
-  /* request.create(temporalObject); */
-}
+    id: id.value,
+    name: name.value,
+    date: {
+      year: parseInt(dateRequest[0]),
+      month: parseInt(dateRequest[1]),
+      day: parseInt(dateRequest[2]),
+    },
+    type: type.value,
+    description: description.value,
+  };
+  request.create(temporalObject);
+};
+
 </script>
 
 <template>
@@ -18,24 +30,24 @@ const postMethod = ()=>{
     <div class="container-form">
       <div class="input-zone">
         <Label class="lable-item">Enter your id</Label>
-        <input class="input-item" type="number" v-model="id"/>
+        <input class="input-item" type="number" v-model="id" />
       </div>
       <div class="input-zone">
         <Label class="lable-item">Enter your Name</Label>
-        <input class="input-item" type="text" />
+        <input class="input-item" type="text" v-model="name" />
       </div>
       <div class="input-zone">
         <Label class="lable-item">Enter the date request</Label>
-        <input class="input-item" type="text" />
+        <input class="input-item" type="text" v-model="date" />
       </div>
       <div class="input-zone">
         <Label class="lable-item">Type of request</Label>
-        <input class="input-item" type="text" />
+        <input class="input-item" type="text" v-model="type" />
       </div>
     </div>
     <div class="description-input">
       <label class="description-label">Description</label>
-      <textarea name="" id="" cols="100" rows="10"></textarea>
+      <textarea cols="100" rows="10" v-model="description"></textarea>
     </div>
     <div class="button-zone">
       <button class="button-send" @click="postMethod()">Send</button>
