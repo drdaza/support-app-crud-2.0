e6473d2c491df47c0a75dcde1e962cb5db029e23
RaulMartinezf5 <raulmartinezalvarez1980@gmail.com>
2023-01-26 12:39:39 +0100

    Form terminado

--- src/main/frontend/src/views/Form.vue
+++ src/main/frontend/src/views/Form.vue
@@ -1,5 +1,12 @@
 <script setup>
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+import FormComponent from "../components/FormComponent.vue";
+
 </script>
 <template>
-    <h1>hola</h1>
+    <Header></Header>
+    <FormComponent></FormComponent>
+    <Footer></Footer>
+
 </template>
